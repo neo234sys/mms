af77c1e310fee73e134ab59f2cfecd8c6ad2c02b
hasan <hasan234abu@gmail.com>
2025-01-08 11:09:41 +0400

    email template folder moved to resource folder

--- mms/src/main/java/com/sbmtech/mms/config/VelocityEngineConfig.java
+++ mms/src/main/java/com/sbmtech/mms/config/VelocityEngineConfig.java
@@ -1,35 +1,38 @@
 package com.sbmtech.mms.config;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.exception.VelocityException;
 import org.apache.velocity.runtime.RuntimeConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-
-import com.sbmtech.mms.service.impl.AppSystemPropImpl;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
-@DependsOn("AppSystemProp")
 public class VelocityEngineConfig {
 
 	@Value("${email.templatePath}")
 	private String emailTemplatePath;
+	
+	@Autowired
+    private ResourceLoader resourceLoader;
+	
 	@Bean
 	VelocityEngine velocityEngine() throws VelocityException, IOException {
-		if (!AppSystemPropImpl.props.isEmpty()) {
-			//String emailTemplatePath = AppSystemPropImpl.props.get("email.templatePath");
-			Properties props = new Properties();
-			props.put(RuntimeConstants.RESOURCE_LOADER, "file");
-			props.put(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, emailTemplatePath);
-			VelocityEngine velocityEngine = new VelocityEngine(props);
-			velocityEngine.init();
-			return velocityEngine;
-		}
-		return new VelocityEngine();
+		Resource resource = resourceLoader.getResource("classpath:" + emailTemplatePath);
+		File templateDir=resource.getFile();
+
+		Properties props = new Properties();
+		props.put(RuntimeConstants.RESOURCE_LOADER, "file");
+		props.put(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, templateDir.toPath().toString());
+		VelocityEngine velocityEngine = new VelocityEngine(props);
+		velocityEngine.init();
+		return velocityEngine;
 	}
 }
